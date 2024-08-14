c03464d1e7610a69ec2dbee36a27743ed752c4ff
Pasindu Malshan <e20244@eng.pdn.ac.lk>
2024-08-14 09:16:41 +0530

    Setup

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
---
+++ src/main/java/com/example/demo/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.dto.EmployeeDto;
+import com.example.demo.entity.Employee;
+import com.example.demo.mapper.EmployeeMapper;
+import com.example.demo.repository.EmployeeRepository;
+import com.example.demo.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+    private EmployeeRepository employeeRepository;
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee= EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee=employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+}
